1f8b9460c22a9ac1ff45d721afbbf7f5d7a55d9a
Francisco Giordano <fg@frang.io>
2025-05-11 03:49:55 +0100

    add json streaming lexer

---
+++ src/lib/json-stream.test.ts
@@ -0,0 +1,46 @@
+import { test, suite } from 'node:test';
+import assert from 'node:assert/strict';
+import { lexer, type JsonToken } from './json-stream.ts';
+
+function lex(chunks: string[]): JsonToken[] {
+  let lex = lexer();
+  return [...chunks, undefined].flatMap(chunk => lex(chunk));
+}
+
+suite('json stream lexer', () => {
+  test('simple JSON object', () => {
+    const tokens = lex(['{"key":', ' "value"}']);
+    assert.deepEqual(tokens, [
+      { type: 'begin-object', startChunk: 0, startIndex: 0, endChunk: 0, endIndex: 1 },
+      { type: 'atom', startChunk: 0, startIndex: 1, endChunk: 0, endIndex: 6 },
+      { type: 'name-separator', startChunk: 0, startIndex: 6, endChunk: 0, endIndex: 7 },
+      { type: 'atom', startChunk: 1, startIndex: 1, endChunk: 1, endIndex: 8 },
+      { type: 'end-object', startChunk: 1, startIndex: 8, endChunk: 1, endIndex: 9 },
+    ]);
+  });
+
+  test('split string', () => {
+    const tokens = lex(['"Hello', ' World"']);
+    assert.deepEqual(tokens, [
+      { type: 'atom', startChunk: 0, startIndex: 0, endChunk: 1, endIndex: 7 },
+    ]);
+  });
+
+  test('lone quotes', () => {
+    const tokens = lex(['"', '" "', '"']);
+    assert.deepEqual(tokens, [
+      { type: 'atom', startChunk: 0, startIndex: 0, endChunk: 1, endIndex: 1 },
+      { type: 'atom', startChunk: 1, startIndex: 2, endChunk: 2, endIndex: 1 },
+    ]);
+  });
+
+  test('escapes', () => {
+    assert.deepEqual(lex(['"\\""']), [
+      { type: 'atom', startChunk: 0, startIndex: 0, endChunk: 0, endIndex: 4 },
+    ]);
+
+    assert.deepEqual(lex(['"\\', '""']), [
+      { type: 'atom', startChunk: 0, startIndex: 0, endChunk: 1, endIndex: 2 },
+    ]);
+  });
+});
---
+++ src/lib/json-stream.ts
@@ -0,0 +1,128 @@
+const JSON_TOKEN_PREFIX = /[ \t\n\r]*()(?:[{}\[\]:,]|"(?:[^\\"]|\\(?:.|$))*(?:"|$)|[.\-\w]+)?/dy;
+const JSON_STRING_CONT = /(?:[^\\"]|\\(?:.|$))*(?:"|$)/y;
+const JSON_NS_ATOM_CONT = /[.\-\w]*/y; // non-string atom
+
+export type JsonTokenType =
+  | 'begin-object'
+  | 'end-object'
+  | 'begin-array'
+  | 'end-array'
+  | 'value-separator'
+  | 'name-separator'
+  | 'atom'; // strings, numbers, booleans, null
+
+export interface JsonToken {
+  type: JsonTokenType;
+  startChunk: number;
+  startIndex: number;
+  endChunk: number;
+  endIndex: number;
+}
+
+const JSON_TOKEN_TYPE_MAP: Record<string, JsonTokenType> = {
+  '{': 'begin-object',
+  '}': 'end-object',
+  '[': 'begin-array',
+  ']': 'end-array',
+  ',': 'value-separator',
+  ':': 'name-separator',
+};
+
+export function lexer(): (chunk?: string) => JsonToken[] {
+  let chunkIndex = 0;
+
+  let pending = JSON_STRING_CONT;
+  let pendingStart = 0;
+  let pendingToken: JsonToken | undefined;
+
+  return function (chunk?: string): JsonToken[] {
+    let tokens: JsonToken[] = [];
+
+    if (chunk === undefined) {
+      if (pendingToken !== undefined) {
+        tokens.push(pendingToken);
+        pendingToken = undefined;
+      }
+      return tokens;
+    }
+
+    JSON_TOKEN_PREFIX.lastIndex = 0;
+
+    if (pendingToken !== undefined) {
+      pending.lastIndex = pendingStart;
+
+      let match = pending.exec(chunk);
+
+      if (match === null) {
+        throw 'todo: error in pending';
+      }
+
+      let endIndex = pending.lastIndex;
+      let lastSymbol = chunk[endIndex - 1]!;
+
+      pendingToken.endChunk = chunkIndex;
+      pendingToken.endIndex = endIndex;
+
+      if (endIndex === chunk.length && lastSymbol !== '"') {
+        pendingStart = lastSymbol === '\\' ? 1 : 0;
+        return tokens;
+      } else {
+        tokens.push(pendingToken);
+        pendingToken = undefined;
+
+        JSON_TOKEN_PREFIX.lastIndex = endIndex;
+      }
+    }
+
+    while (JSON_TOKEN_PREFIX.lastIndex < chunk.length) {
+      let match = JSON_TOKEN_PREFIX.exec(chunk);
+
+      if (match === null) {
+        throw 'todo: error';
+      }
+
+      if (match.indices![1] === undefined) {
+        break;
+      }
+
+      let endIndex = JSON_TOKEN_PREFIX.lastIndex;
+      let [startIndex] = match.indices![1];
+      let symbol = chunk[startIndex]!;
+      let type = JSON_TOKEN_TYPE_MAP[symbol] ?? 'atom';
+      let token = {
+        type,
+        startChunk: chunkIndex,
+        startIndex,
+        endChunk: chunkIndex,
+        endIndex,
+      };
+
+      switch (type) {
+        case 'atom': {
+          if (symbol === '"') {
+            let lastSymbol = chunk[endIndex - 1]!;
+            if (endIndex == startIndex + 1 || lastSymbol !== '"') {
+              pending = JSON_STRING_CONT;
+              pendingStart = lastSymbol === '\\' ? 1 : 0;
+              pendingToken = token;
+              break;
+            }
+          } else if (endIndex === chunk.length) {
+            pending = JSON_NS_ATOM_CONT;
+            pendingStart = 0;
+            pendingToken = token;
+            break;
+          }
+        }
+
+        default: {
+          tokens.push(token);
+        }
+      }
+    }
+
+    chunkIndex += 1;
+
+    return tokens;
+  }
+}
